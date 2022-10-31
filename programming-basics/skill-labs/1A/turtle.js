de3bdd6cfd786257335c33931e9e53e1fedd90c6
DutchSparklez <wschaijk@outlook.com>
2022-10-31 07:09:17 +0100

    Skills Lab 1A

---
+++ programming-basics/skill-labs/1A/turtle.js
@@ -0,0 +1,62 @@
+/// <reference path="./lib/Intellisense/js-turtle_hy.ts" />
+//DOCUMENTATION: https://hanumanum.github.io/js-turtle/
+
+// Setup
+showGrid(50); 
+setSpeed(200);
+width(5);
+
+// Square
+color(255, 0, 0);
+forward(250);
+right(90);
+forward(250);
+right(90);
+forward(250);
+right(90);
+forward(250);
+right(90);
+
+// Triangle
+color(0, 255, 0);
+left(30);
+forward(100);
+left(120);
+forward(100);
+left(120);
+forward(100);
+
+// Hexagon
+color(0, 0, 255);
+forward(75);
+right(60);
+forward(150);
+right(60);
+forward(150);
+right(60);
+forward(150);
+right(60);
+forward(150);
+right(60);
+forward(150);
+right(60);
+forward(75);
+
+// My first letter
+penup();
+goto(-50, -50);
+right(90);
+pendown();
+color(255, 0, 255);
+forward(125);
+left(90);
+forward(50);
+left(90);
+forward(75);
+left(180);
+forward(75);
+left(90);
+forward(50);
+left(90);
+forward(125);
+
