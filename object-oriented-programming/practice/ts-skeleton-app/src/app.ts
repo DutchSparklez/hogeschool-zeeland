ba0e82624ea2664aaad3d51e2f6d665e06aef5dc
DutchSparklez <wschaijk@outlook.com>
2022-11-14 22:06:14 +0100

    Initiate OOP

--- object-oriented-programming/practice/ts-skeleton-app/src/app.ts
+++ object-oriented-programming/practice/ts-skeleton-app/src/app.ts
@@ -2,7 +2,7 @@ import HelloWorld from './HelloWorld.js';
 
 const greeting = new HelloWorld();
 console.log(greeting.greet());
+
 window.addEventListener('load', () => {
   document.getElementById('content').innerText = greeting.greet();
-}
-);
+});
